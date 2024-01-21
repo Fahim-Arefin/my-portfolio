790e3a578d26c951587443a3a171376c3ff3c0e3
Fahim <fahimarefin57@gmail.com>
2024-01-22 00:13:19 +0600

    Modal updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -60,6 +60,7 @@ function Contact() {
         <div className="modal-box bg-[#112240] text-[#64ffdb]">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
+            {/* if there is a button in form, it will close the modal */}
             <button
               onClick={() => modalRef.current.close()}
               className="btn btn-sm btn-circle btn-ghost absolute right-3 top-3"
